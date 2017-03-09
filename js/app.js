5ed3a33b4e69c91e018f17d041f72ba989450de3
Will Hawkins <willhawkins123@yahoo.com>
2017-03-08 21:06:58 -0500

    Index and Show

--- js/app.js
+++ js/app.js
@@ -1,5 +1,44 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+    .module("wdinstagram", [
+      "ui.router"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ])
+    .controller("PostsIndexController", [
+      PostsIndexControllerFucntion
+    ])
+    .controller("PostsShowController", [
+      "$stateParams",
+      PostsShowControllerFunction
+    ])
+
+    function RouterFunction($stateProvider){
+      $stateProvider
+        .state("postsIndex", {
+          url: "/posts",
+          templateUrl: "js/ng-views/index.html",
+          controller: "PostsIndexController",
+          controllerAs: "vm"
+        })
+        .state("postShow", {
+          url: "posts/:id",
+          templateUrl: "js/ng-views/show.html",
+          controller: "PostsShowController",
+          controllerAs: "vm"
+        })
+    }
+
+    function PostsIndexControllerFucntion(){
+      this.posts = seedData;
+    }
+
+    PostsShowControllerFunction.$inject = ["$stateParams"]
+    function PostsShowControllerFunction($stateParams){
+      this.post = seedData[$stateParams.id]
+    }
 })();
