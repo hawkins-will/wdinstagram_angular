06d4d58062026b5fbe2a00c06963af16259b160e
Will Hawkins <willhawkins123@yahoo.com>
2017-03-08 22:10:33 -0500

    I'm so tired

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
 "use strict";
 
-(function(){
   angular
     .module("wdinstagram", [
-      "ui.router"
+      "ui.router",
+      "ngResource"
     ])
     .config([
       "$stateProvider",
       RouterFunction
     ])
+    .factory("PostFactory", [
+      "$resource",
+      PostFactoryFunction
+    ])
     .controller("PostsIndexController", [
+      "PostFactory",
       PostsIndexControllerFucntion
     ])
     .controller("PostsShowController", [
+      "PostFactory",
       "$stateParams",
       PostsShowControllerFunction
+    ])
+    .controller("PostsEditController", [
+      "PostFactory",
+      "$stateParams",
+      "$state",
+      PostsEditControllerFunction
     ])
 
     function RouterFunction($stateProvider){
@@ -31,14 +43,45 @@
           controller: "PostsShowController",
           controllerAs: "vm"
         })
+        .state("postEdit", {
+          url: "/grams/:id/edit",
+          templateUrl: "js/ng-views/edit.html",
+          controller: "PostsEditController",
+          controllerAs: "vm"
+        })
     }
 
-    function PostsIndexControllerFucntion(){
-      this.posts = seedData;
+    function PostFactoryFunction($resource){
+      return  $resource("http://localhost:3000/entries/:id", {}, {
+        update: {method: "PUT"}
+      })
     }
 
-    PostsShowControllerFunction.$inject = ["$stateParams"]
-    function PostsShowControllerFunction($stateParams){
-      this.post = seedData[$stateParams.id]
+    function PostsIndexControllerFucntion(PostFactory, $state){
+      this.posts = PostFactory.query();
+
+      this.post = new PostFactory();
+      this.create = function(){
+        this.post.$save(function(post) {
+          $state.go("postShow", {id: post.id})
+        })
+      }
     }
-})();
+
+    function PostsShowControllerFunction(PostFactory, $stateParams){
+      this.post = PostFactory.get({id: $stateParams.id});
+    }
+
+    function PostsEditControllerFunction(PostFactory, $stateParams, $state) {
+      this.post = PostFactory.get({id: $stateParams.id})
+      this.update = function(){
+        this.post.$update({id: $stateParams.id}, function(gram) {
+          $state.go("postShow", {id: post.id})
+        })
+      }
+      this.destroy = function(){
+        this.post.$delete({id: $stateParams.id}, function() {
+          $state.go("postsIndex")
+        })
+      }
+    }
